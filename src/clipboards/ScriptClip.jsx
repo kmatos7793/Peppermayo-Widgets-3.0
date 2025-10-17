8d6915fdfdf636e34e6ced8a52a9bf9f87d7305d
Kenneth John Matos <kmatos@yotpo.com>
2025-10-17 20:23:44 +0800

    Peppermayo Widgets 3.0

--- src/clipboards/ScriptClip.jsx
+++ src/clipboards/ScriptClip.jsx
@@ -3,7 +3,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function ScriptClipBoard() {
   const [value, setValue] = useState(`
-  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/LH9SK0pxFsNGa4Xmsxk4mH8W3EXqzi1qRUdtSOPz" async></script>
+  <script src="https://cdn-widgetsrepository.yotpo.com/v1/loader/spM3pEHyrxrVmDUXBeGBRiTxdYPFqObcqwWCESZ4" async></script>
     `);
   const [isCopied, setCopied] = useState(false);
 
